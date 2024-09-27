28e1ccb6fee6ece32674b7ed667147fee1aa6070
ramiro1996 <ramiro.lc12@yahoo.com>
2024-09-26 21:27:59 -0600

    Fix carp control

---
+++ CP_Control/ManejadorBDSQL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP_Control.CP_Control
+{
+    class ManejadorBDSQL:ManejadorBaseDatos
+    {
+        public ManejadorBDSQL()
+        {
+           sqlconex = new SqlConnection(@"Data Source=AMTI00288-LAPTO; initial catalog=Carpitectura_Pizana; Integrated Security=true;");
+        
+        }
+
+        SqlConnection sqlconex;
+        SqlCommand sqlcom;
+         public override System.Data.DataSet ConsultarTablaBD(string consulta)
+        {
+            try
+            {
+                System.Data.DataSet dataSet1 = new System.Data.DataSet();
+                sqlcom = new SqlCommand(consulta, sqlconex);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                sqlconex.Open();
+                dataAdapter.SelectCommand = sqlcom;
+                dataAdapter.Fill(dataSet1, "Id");
+                sqlconex.Close();
+                return (dataSet1);
+            }
+            catch { sqlconex.Close(); return null; }
+        }
+        public override object ConsultaEscalarBD(string consulta)
+        {
+            object valor;
+            sqlcom = new SqlCommand(consulta, sqlconex);
+            sqlconex.Open();
+            valor = sqlcom.ExecuteScalar();
+            sqlconex.Close();
+            return valor;
+        }
+        public override void InsertaModificaBD(string consulta)
+        {
+            sqlcom = new SqlCommand(consulta, sqlconex);
+            sqlconex.Open();
+            sqlcom.ExecuteNonQuery();
+            sqlconex.Close();
+        }
+    }
+}
---
+++ CP_Control/ManejadorBaseDatos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP_Control.CP_Control
+{
+   abstract  class ManejadorBaseDatos
+    {
+        public abstract System.Data.DataSet ConsultarTablaBD(string consulta);
+        public abstract object ConsultaEscalarBD(string consulta);
+        public abstract void InsertaModificaBD(string consulta);
+    }
+}
